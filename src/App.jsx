d9acf39b5bd5a72a28572524c94b86093cb6d9ed
Jeongmin Oh <ojm51@naver.com>
2025-08-19 12:43:05 +0900

    feat: 일기 생성, 수정, 삭제 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useReducer, useRef } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
@@ -6,7 +7,58 @@ import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.data];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data.id));
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [diaryState, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(3);
+
+  const onCreate = (createdDate, emoitonId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emoitonId,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id, createdDate, emoitonId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id: id,
+        createdDate,
+        emoitonId,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: {
+        id: id,
+      },
+    });
+  };
+
   return (
     <>
       <Routes>
