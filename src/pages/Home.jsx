a41320871816d2a124ae62f9bfe44ac8ac91f9cd
Jeongmin Oh <ojm51@naver.com>
2025-08-28 22:56:13 +0900

    feat: 홈페이지 날짜 변경 버튼 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
+import { useState } from "react";
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const handleDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  const handleIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2025년 8월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={"<"} onClick={handleDecreaseMonth} />}
+        rightChild={<Button text={">"} onClick={handleIncreaseMonth} />}
       />
 
       <DiaryList />
