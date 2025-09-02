ee4937b2bda381557e9fae2cc13c979315f15a51
Jeongmin Oh <ojm51@naver.com>
2025-09-02 23:00:32 +0900

    design: 일기 상세 페이지 ui 구현

---
+++ src/components/Viewer.jsx
@@ -0,0 +1,33 @@
+import "./Viewer.css";
+import getEmotionImage from "../utils/getEmotionImage";
+import emotionList from "../contents/emotionList";
+
+const Viewer = ({ emotionId, content }) => {
+  const emotionItem = emotionList.find(
+    (item) => String(item.emotionId) === String(emotionId)
+  );
+
+  return (
+    <div className="Viewer">
+      <section className="img_section">
+        <h4>오늘의 감정</h4>
+        <div className={`emotion_img_wrapper emotion_img_wrapper_${emotionId}`}>
+          <img
+            src={getEmotionImage(emotionId)}
+            alt={`${emotionId}번 감정 이미지`}
+          />
+          <div>{emotionItem.emotionName}</div>
+        </div>
+      </section>
+
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <div className="content_wrapper">
+          <p>{content}</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Viewer;
