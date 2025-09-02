723932e4947da184c42c948e72f25d3ef1907cf6
Jeongmin Oh <ojm51@naver.com>
2025-09-02 15:29:20 +0900

    feat: 일기 삭제 기능 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -10,6 +10,7 @@ const Edit = () => {
   const nav = useNavigate();
 
   const diaryData = useContext(DiaryStateContext);
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
   const [initData, setInitData] = useState();
 
   useEffect(() => {
@@ -25,12 +26,21 @@ const Edit = () => {
     setInitData(currentDiaryData);
   }, [params.id]);
 
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
   return (
     <div>
       <Header
         title={"일기 수정하기"}
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
-        rightChild={<Button text={"삭제하기"} type={"negative"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"negative"} />
+        }
       />
       <Editor initData={initData} />
     </div>
