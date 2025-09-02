6def6969b51e7f271cc62aaca46a95e436eb7340
Jeongmin Oh <ojm51@naver.com>
2025-09-02 15:25:46 +0900

    feat: 각 input 초깃값 불러오기

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,19 +1,38 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 
 const Edit = () => {
   const params = useParams();
+  const nav = useNavigate();
+
+  const diaryData = useContext(DiaryStateContext);
+  const [initData, setInitData] = useState();
+
+  useEffect(() => {
+    const currentDiaryData = diaryData.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryData) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+
+    setInitData(currentDiaryData);
+  }, [params.id]);
 
   return (
     <div>
       <Header
         title={"일기 수정하기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
         rightChild={<Button text={"삭제하기"} type={"negative"} />}
       />
-      <Editor />
+      <Editor initData={initData} />
     </div>
   );
 };
