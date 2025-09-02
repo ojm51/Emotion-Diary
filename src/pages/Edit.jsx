4bf9c0c613d5675e06dfc740792b44d4fbcc27d5
Jeongmin Oh <ojm51@naver.com>
2025-09-02 15:04:56 +0900

    design: 수정 페이지 ui 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const Edit = () => {
   const params = useParams();
 
-  return <div>{params.id}th Edit page</div>;
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+        rightChild={<Button text={"삭제하기"} type={"negative"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
